78eb9ddd76b8dbeefaf38904c29333c7e854aa45
Leisa Bryan <lljbryan123@gmail.com>
2019-10-17 18:59:57 -0700

    m

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,13 @@ var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
 
 
+
 function getPasswordOptions() {
     var length = parseInt(
         prompt("How many characters would you like your password to contain?")
     );
+   
+
 
     if (isNaN(length)) {
         alert("Password length must be a number");
@@ -51,12 +54,7 @@ function getPasswordOptions() {
         uppercaseChars: uppercaseChars,
         lowercaseChars: lowercaseChars
     };
-    // var passwordOptions = {
-    //   specialChars,
-    //   numericChars,
-    //   uppercaseChars,
-    //   lowercaseChars
-    // };
+  
 
     return passwordOptions;
 
@@ -75,54 +73,10 @@ if(passwordOptions.specialChars) {
 
 }
 
-
-// function getPasswordOptions() {
-//     var length = parseInt(
-//         prompt("How many characters would you like your password to contain?")
-//     )
-// };
-
-// if (isNaN(length)) {
-//     alert("Password length must be a number");
-//     return;
-// }
-// if (length < 8) {
-//     alert("Password length must be greater than 8";
-//     return
-// }
-// if (length < 128) {
-//     alert("Password length must be less than 128");
-//     return
-// }
-
-// var specialChars = contirm("Would you like to use special character?");
-// return;
-
-// var numericChars = contirm("Would you like to use numeric character?");
-// return;
-
-// var uppercaseChars = contirm("Would you like to use uppercasecharacter?");
-// return;
-
-// var lowercaseChars = contirm("Would you like to use lowercase character?");
-// return;
-
-// if (!specialChars && !numericChars && !uppercaseChars && !lowercaseChars) {
-//     alert("We can't help you, you are too picky...")
-
-//     var passwordOptions = {
-//         specialChars: specialChars,
-//         numbericChars: numericChars,
-//         uppercaseChars: uppercaseChars,
-//         lowercaseChars: lowercaseChars
-//     };
-
-//     console.log("We Made it!")
+console.log("your password is" + specialChars + numericChars + uppercaseChars + loweraseChars) 
 
 
-// }
-
-// getPasswordOptions();
+ console.log("We Made it!")
 
 
 
@@ -135,22 +89,24 @@ if(passwordOptions.specialChars) {
 
 
 
-// varInputUpperCase = prompt ("Input Capital letter") ||
-// varInputLowerCase = prompt ("Input lowercase letter") ||
-// varInputNum = Prompt("Choose # 1 - 9") ||
-// varInputSpecialCharacter = prompt("Input Special Character")
 
-// for (var i = 0; i < myLowercase.length, i++){
-//     console.log ("I chose letter" = myLowercase[i])
-// }
-// for (var i = 0; i < myLowercase.length, i++){
-//     console.log ("I chose letter" = myLowercase[i])
-// }
-// for (var i = 0; i < myLowercase.length, i++){
-//     console.log ("I chose letter" = myLowercase[i])
-// }
-// for (var i = 0; i < myLowercase.length, i++){
-//     console.log ("I chose letter" = myLowercase[i])
-// }
 
-// alert("Your total is: $" + totalCost);
+
+varInputUpperCase = prompt ("Input Capital letter") ||
+varInputLowerCase = prompt ("Input lowercase letter") ||
+varInputNum = Prompt("Choose # 1 - 9") ||
+varInputSpecialCharacter = prompt("Input Special Character")
+
+for (var i = 0; i < myLowercase.length, i++){
+console.log ("I chose letter" = myLowercase[i])
+}
+for (var i = 0; i < myLowercase.length, i++){
+    console.log ("I chose letter" = myLowercase[i])
+}
+for (var i = 0; i < myLowercase.length, i++){
+    console.log ("I chose letter" = myLowercase[i])
+}
+ for (var i = 0; i < myLowercase.length, i++){
+    console.log ("I chose letter" = myLowercase[i])
+}
+
